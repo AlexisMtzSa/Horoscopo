8bbb2d09a0ebbb08e53efb90fe2472ea5e7253cf
AlexisMtz <alexismtzsa@hotmail.com>
2019-02-06 08:00:05 -0600

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,62 @@
+function aries(){
+    var imagen = document.getElementById('img');
+    imagen.src = "img/1aries/aries.jpg";
+    document.getElementById('caja-texto').innerHTML="Para crecer a nivel profesional, es importante que este mes Aries satisfaga esas inquietudes laborales que le permitan ampliar sus horizontes, con ello, Aries mejorará su productividad en el trabajo que le incitará para alcanzar el éxito este 2019. Habrá hoy cierta situación en el entorno personal de los nacidos bajo este signo del Zodiaco que le hará tener una actitud distante con algunas personas, incluso con esa persona que le ha despertado algo más que atracción, si Aries tiene pareja, la posición de los astros le mostrará una nueva perspectiva si muestra sus verdaderos sentimientos, con ello, resurgirá el amor. Para gozar de buena salud será conveniente que Aries cambie la comida chatarra por algo más saludable.";
+}   
+function tauro(){
+    var imagen = document.getElementById('img');
+    imagen.src = "img/2tauro/tauro.jpg";
+    document.getElementById('caja-texto').innerHTML="Con la estabilidad laboral y económica de Tauro, este mes será importante empezar a enfocarse en nuevas propuestas que le aporten ciertos desafíos en el trabajo para despertar esa ambición que se está disipando, esto generará que Tauro progrese en sus objetivos con buenas expectativas profesionales. Con la energía de los astros a su favor, los nacidos bajo este signo del Zodiaco, deberían estar atentos en su entorno, ya que el amor podría estar más cerca de lo que piensa, para que llegue, será conveniente que Tauro esté dispuesto a dar un paso y cerrar ciclos sentimentales para que entren nuevas emociones, si tiene una relación, este mes será un periodo de mucho movimiento en el terreno sentimental, que le hará dar un giro positivo a su vida en pareja.";
+}   
+function geminis(){
+    var imagen = document.getElementById('img');
+    imagen.src = "img/3geminis/geminis.jpg";
+    document.getElementById('caja-texto').innerHTML="Si Géminis hace los movimientos necesarios incitará nuevas oportunidades profesionales que le harán tener mayor confianza en el trabajo para tomar decisiones, lo que será ideal para enfrentarse a estos desafíos con los cuales Géminis podrá satisfacer este mes ciertos objetivos laborales. El buen humor será la clave para que hoy los nacidos bajo este signo de Zodiaco amplíen su círculo de amigos, ideal si está soltero, ya que la alineación de los astros incitará para que el amor surja en cualquier momento, si tiene pareja, febrero será un mes intenso para la relación de Géminis.  Aprender a relajarse evitará males mayores a la salud de los nacidos bajo este signo del Zodiaco, el ejercicio será una buena forma de combatir el estrés del día a día.";
+}   
+function cancer(){
+    var imagen = document.getElementById('img');
+    imagen.src = "img/4cancer/cancer.jpg";
+    document.getElementById('caja-texto').innerHTML="Será conveniente que en el trabajo Cáncer no pierda detalle de lo que acontece, porque marcará un antes y un después en su carrera, esto hará que Cáncer actúe, estos primeros días de febrero, con habilidad para evitar salir perjudicado en el entorno laboral y económico. La posición de los astros estará este mes a favor de los nacidos bajo este signo del Zodiaco, por ello, será un mes en el cual reinará el amor, tanto si tiene o no pareja, aunque no deberá descuidar la comunicación que será clave para mantener esa estabilidad que Cáncer vive en su relación, si está soltero, el amor surgirá tan pronto como deje a un lado su pasado sentimental. Aunque la salud de Cáncer, será estable este mes, hoy será recomendable relajarse y no tomarse todo tan apecho, que solo afectará su sistema nervioso.";
+}   
+function leo(){
+    var imagen = document.getElementById('img');
+    imagen.src = "img/5leo/leo.jpg";
+    document.getElementById('caja-texto').innerHTML="Leo iniciará febrero con algunas situaciones complicadas en el trabajo, que podrían absorber más tiempo del pensado, será un periodo estresante si Leo no actúa con destreza y habilidad, para ello, la confianza le hará tomar las decisiones más adecuadas y ser determinante en sus objetivos profesionales. Los nacidos bajo este signo del Zodiaco entrarán en una nueva etapa sentimental que le permitirá sentir ciertas emociones que gozará si disfruta el momento, este mes, es tiempo para que Leo, tanto si tiene o no pareja tenga una perspectiva diferente en el amor, esto le hará estar de buen humor y ampliar su circulo de amigos, será un mes muy positivo a nivel personal. Los astros auguran buena salud para Leo, pero deberá salir de la rutina para ver los beneficios en muchos sentidos.";
+}   
+function virgo(){
+    var imagen = document.getElementById('img');
+    imagen.src = "img/6virgo/virgo.jpg";
+    document.getElementById('caja-texto').innerHTML="Febrero será un mes muy productivo para Virgo si mantiene despierta su ambición que le permitirá alcanzar sus objetivos profesionales, aunque será un desafío con el cual Virgo debería actuar con habilidad para no perder el enfoque de su propósito en el trabajo ni el dinero. La estabilidad emocional de los nacidos bajo este signo del Zodiaco será influida por la conjunción de los astros, por ello, hoy será un día ideal para disfrutar de la familia y amigos, donde podría surgir el amor, si tiene pareja, la confianza y comunicación serán claves para que Virgo fortalezca su relación. El deporte ayudará a tonificar el cuerpo, además dará a Virgo beneficios a su salud.";
+}   
+function libra(){
+    var imagen = document.getElementById('img');
+    imagen.src = "img/7libra/libra.jpg";
+    document.getElementById('caja-texto').innerHTML="Libra se verá obligado esta semana a posponer algunos asuntos de trabajo para enfocarse a ciertos proyectos de urgencia que tendrán que estar preparados para mediados de mes, esto permitirá que Libra progrese con éxito en el terreno profesional. febrero empezará con cierta disposición de los astros en favorecer a los nacidos bajo este signo del Zodiaco en el amor, por ello, será un mes en el cual el estado emocional de los nacidos bajo este signo del Zodiaco estarán más estable, por ello sus sentimientos volverán a su cauce a partir de hoy, esto permitirá que la relación con su pareja inicie una nueva etapa de comunicación y confianza que les fortalecerá, si está soltero, Libra empezará a sentir nuevas emociones hacia alguien muy cercano.";
+}   
+function escorpion(){
+    var imagen = document.getElementById('img');
+    imagen.src = "img/8escorpion/escorpion.jpg";
+    document.getElementById('caja-texto').innerHTML="Los cambios y las novedades que se avecinan en el trabajo para Escorpio, harán que este mes se plantee nuevos objetivos, cuyas expectativas serán altas, esto despertará la ambición profesional de Escorpio que le hará progresar en su carrera. La vida social de los nacidos bajo este signo del Zodiaco estará en auge este fin de semana, esto propiciará que el buen humor se potencie y despierte cierta atracción en alguien que tarará de conquistarle, si tiene pareja, hoy será un día tranquilo, aunque la estabilidad en el amor sea positiva, los astros recomiendan a Escorpio no caer en la rutina que disipe los sentimientos en su relación. Aunque su salud física esté en plenitud, este mes será ideal para que Escorpio ponga atención a su salud emocional.";
+}   
+function sagitario(){
+    var imagen = document.getElementById('img');
+    imagen.src = "img/9sagitario/sagitario.jpg";
+    document.getElementById('caja-texto').innerHTML="Febrero será un mes muy productivo y positivo para Sagitario en el trabajo, el esfuerzo y dedicación empezarán a dar resultado, por ello, será un periodo en el cual aparecerán grandes oportunidades de éxito profesional para Sagitario, ahora toca decidir. Hoy habrá cierta situaciones en su entorno que harán pensar a los nacidos bajo este signo del Zodiaco, será el momento de dar un giro a su vida personal, aunque esto signifique tener una nueva actitud que no será del agrado de muchos, no obstante, esto es lo que permitirá al movimiento de los astros incitar para que surja el amor en el momento menos esperado, si tiene una relación, hoy las palabras y los gestos podrían ser contraproducentes si Sagitario no mantiene una buena comunicación con su pareja.";
+}   
+function capricornio(){
+    var imagen = document.getElementById('img');
+    imagen.src = "img/10capricornio/capricornio.jpg";
+    document.getElementById('caja-texto').innerHTML="Los astros auguran dinero y trabajo para Capricornio este 2019, sin embargo, este mes será clave para actuar con decisión y propiciar esos cambios a los que Capricornio está destinado, por ello, nuevos desafíos surgirán para encaminar hacia terrenos profesionales que le permitan progresar en su carrera. Los cambios de humor de los nacidos bajo este signo del Zodiaco harán que hoy sea un día complicado con su pareja, las discusiones estarán muy presentes, aunque podrá solventarlas con éxito, si está soltero, esta actitud provocará cierto distanciamiento con esa persona especial, tendrá solución si Capricornio deja a un lado el orgullo y permite que el amor surja. Para que el 2019 será un año próspero en salud, Capricornio debería empezar a poner orden equilibrando cuerpo y mente.";
+}   
+function acuario(){
+    var imagen = document.getElementById('img');
+    imagen.src = "img/11acuario/acuario.jpg";
+    document.getElementById('caja-texto').innerHTML="A pesar de los rumores que rondan en el trabajo, será importante que Acuario no pierda el enfoque de sus objetivos, esto le hará centrarse en sus proyectos con mayor compromiso, ya que las expectativas de éxito que rondan a Acuario en el terreno profesional y económico son altas para este mes. La influencia de los astros evitará que hoy los nacidos bajo este signo del Zodiaco tome decisiones precipitadas en su relación, por ello, febrero, será un mes en el cual el amor estará muy presente, dejarse llevar por sus sentimientos, permitirá que Acuario no dé pasos en falso y deje que nuevas ilusiones le hagan sonreír. Una buena alimentación y ejercicio harán que este mes Acuario tenga energía para enfrentar los desafíos que están por llegar.";
+}   
+function piscis(){
+    var imagen = document.getElementById('img');
+    imagen.src = "img/12piscis/piscis.jpg";
+    document.getElementById('caja-texto').innerHTML="Febrero inicia para Piscis con buenas expectativas en el trabajo, se generarán grandes oportunidades que deberá aprovechar y sacar ventaja para abrirse camino hacia nuevos horizontes, en los cuales Piscis podría progresar con éxito en su carrera. En el terreno personal, será un mes muy ajetreado entre amigos y familia, empezando hoy con eventos que podrían agobiar a los nacidos bajo este signo del Zodiaco, aunque esto podría cambiar ya que la alineación de los astros le acercará al amor, si Piscis tiene una relación, será un día complicado, pero el amor hará que cualquier altercado pase inadvertido y se fortalezca esos sentimientos que les une como pareja. El deporte que está practicando Piscis empezará este mes a dar beneficios para su salud.";
+}   
+
+
